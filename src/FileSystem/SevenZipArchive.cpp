1553d64703e5a766ce99612dfbf3ff7ce674116d
Marek Rusinowski <marekrusinowski@gmail.com>
2022-12-29 20:03:20 +0100

    Update 7z lib to latest version 22.01 and improve usage
    - Makes sure that there isn't any buffer overflow possible in Utf16_To_Utf8
    - Align implementation more with 7zMain
    - Change 7z CMake target name to 7zip

--- src/FileSystem/SevenZipArchive.cpp
+++ src/FileSystem/SevenZipArchive.cpp
@@ -3,79 +3,91 @@
 #include "SevenZipArchive.h"
 
 #include <algorithm>
-#include <stdexcept>
-#include <string.h>  //memcpy
+#include <cassert>
+#include <cstring>
+#include <optional>
 
-extern "C" {
-#include "lib/7z/7zAlloc.h"
-#include "lib/7z/7zCrc.h"
-#include "lib/7z/7zTypes.h"
-}
+#include <7zAlloc.h>
+#include <7zCrc.h>
 
 #include "Logger.h"
 #include "Util.h"
 
 static Byte kUtf8Limits[5] = {0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
+
+/**
+ * Converts from UTF16 to UTF8. The destLen must be set to the size of the dest.
+ * If the function succeeds, destLen contains the number of written bytes and dest
+ * contains utf-8 \0 terminated string.
+ */
 static bool Utf16_To_Utf8(char* dest, size_t* destLen, const UInt16* src, size_t srcLen)
 {
-	size_t destPos = 0, srcPos = 0;
-	for (;;) {
+	size_t destPos = 0;
+	size_t srcPos = 0;
+	while (destPos < *destLen) {
 		unsigned numAdds;
+		UInt32 value;
+
 		if (srcPos == srcLen) {
+			dest[destPos] = '\0';
 			*destLen = destPos;
-			return True;
+			return true;
 		}
-		UInt32 value = src[srcPos++];
-		if (value < 0x80) {
-			if (dest)
-				dest[destPos] = (char)value;
-			destPos++;
+
+		if ((value = src[srcPos++]) < 0x80) {
+			dest[destPos++] = (char)value;
 			continue;
 		}
+
 		if (value >= 0xD800 && value < 0xE000) {
 			if (value >= 0xDC00 || srcPos == srcLen)
 				break;
+
 			const UInt32 c2 = src[srcPos++];
+
 			if (c2 < 0xDC00 || c2 >= 0xE000)
 				break;
+
 			value = (((value - 0xD800) << 10) | (c2 - 0xDC00)) + 0x10000;
 		}
 		for (numAdds = 1; numAdds < 5; numAdds++)
 			if (value < (((UInt32)1) << (numAdds * 5 + 6)))
 				break;
-		if (dest)
-			dest[destPos] = (char)(kUtf8Limits[numAdds - 1] + (value >> (6 * numAdds)));
-		destPos++;
-		do {
+
+		dest[destPos++] = (char)(kUtf8Limits[numAdds - 1] + (value >> (6 * numAdds)));
+
+		while (destPos < *destLen && numAdds > 0) {
 			numAdds--;
-			if (dest)
-				dest[destPos] = (char)(0x80 + ((value >> (6 * numAdds)) & 0x3F));
-			destPos++;
-		} while (numAdds != 0);
+			dest[destPos++] = (char)(0x80 + ((value >> (6 * numAdds)) & 0x3F));
+		}
 	}
-	*destLen = destPos;
+
 	return false;
 }
 
-int CSevenZipArchive::GetFileName(const CSzArEx* db, int i)
+static std::optional<std::string> GetFileName(const CSzArEx* db, int i)
 {
-	const size_t len = SzArEx_GetFileNameUtf16(db, i, nullptr);
+	constexpr size_t bufferSize = 2048;
+	uint16_t utf16Buffer[bufferSize];
+	char tempBuffer[bufferSize];
 
-	if (len > tempBufSize) {
-		SzFree(nullptr, tempBuf);
-		tempBufSize = len;
-		tempBuf = (UInt16*)SzAlloc(nullptr, tempBufSize * sizeof(tempBuf[0]));
-		if (tempBuf == 0) {
-			return SZ_ERROR_MEM;
-		}
+	// caller has archiveLock
+	const size_t utf16len = SzArEx_GetFileNameUtf16(db, i, nullptr);
+	if (utf16len >= bufferSize)
+		return std::nullopt;
+
+	SzArEx_GetFileNameUtf16(db, i, utf16Buffer);
+	size_t tempBufferLen = bufferSize;
+	if (!Utf16_To_Utf8(tempBuffer, &tempBufferLen, utf16Buffer, utf16len - 1)) {
+		return std::nullopt;
 	}
-	tempBuf[len - 1] = 0;
-	return SzArEx_GetFileNameUtf16(db, i, tempBuf);
+
+	return std::string(tempBuffer, tempBufferLen);
 }
 
-const char* CSevenZipArchive::GetErrorStr(int res)
+static inline const char* GetErrorStr(int err)
 {
-	switch (res) {
+	switch (err) {
 		case SZ_OK:
 			return "OK";
 		case SZ_ERROR_FAIL:
@@ -91,20 +103,16 @@ const char* CSevenZipArchive::GetErrorStr(int res)
 		case SZ_ERROR_NO_ARCHIVE:
 			return "Archive not found";
 	}
+
 	return "Unknown error";
 }
 
 CSevenZipArchive::CSevenZipArchive(const std::string& name)
 	: IArchive(name)
+	, allocImp({SzAlloc, SzFree})
+	, allocTempImp({SzAllocTemp, SzFreeTemp})
 {
-	isOpen = false;
-	allocImp.Alloc = SzAlloc;
-	allocImp.Free = SzFree;
-	allocTempImp.Alloc = SzAllocTemp;
-	allocTempImp.Free = SzFreeTemp;
-	constexpr const size_t kInputBufSize((size_t)1 << 18);
-
-	SzArEx_Init(&db);
+	constexpr const size_t kInputBufSize = (size_t)1 << 18;
 
 #ifdef _WIN32
 	WRes wres = InFile_OpenW(&archiveStream.file, s2ws(name).c_str());
@@ -117,17 +125,20 @@ CSevenZipArchive::CSevenZipArchive(const std::string& name)
 	}
 
 	FileInStream_CreateVTable(&archiveStream);
-	LookToRead2_CreateVTable(&lookStream, False);
+	archiveStream.wres = 0;
 
+	LookToRead2_CreateVTable(&lookStream, false);
 	lookStream.realStream = &archiveStream.vt;
+	lookStream.buf = static_cast<Byte*>(ISzAlloc_Alloc(&allocImp, kInputBufSize));
+	assert(lookStream.buf != NULL);
+	lookStream.bufSize = kInputBufSize;
 	LookToRead2_Init(&lookStream);
-	lookStream.buf = NULL;
-	lookStream.buf = (Byte*)ISzAlloc_Alloc(&allocImp, kInputBufSize);
 
 	CrcGenerateTable();
+
 	SzArEx_Init(&db);
 
-	SRes res = SzArEx_Open(&db, &lookStream.vt, &allocImp, &allocTempImp);
+	const SRes res = SzArEx_Open(&db, &lookStream.vt, &allocImp, &allocTempImp);
 	if (res == SZ_OK) {
 		isOpen = true;
 	} else {
@@ -135,31 +146,24 @@ CSevenZipArchive::CSevenZipArchive(const std::string& name)
 		return;
 	}
 
+	fileData.reserve(db.NumFiles);
+
 	// Get contents of archive and store name->int mapping
 	for (unsigned int i = 0; i < db.NumFiles; ++i) {
-		const bool isDir = SzArEx_IsDir(&db, i);
-
-		if (isDir) {
+		if (SzArEx_IsDir(&db, i)) {
 			continue;
 		}
 
-		const int written = GetFileName(&db, i);
-		if (written <= 0) {
-			LOG_ERROR("Error getting filename in Archive: %s %d, file skipped in %s",
-			          GetErrorStr(res), res, name.c_str());
+		auto fileName = GetFileName(&db, i);
+		if (!fileName) {
+			LOG_ERROR("Error getting filename in Archive, file skipped in %s", name.c_str());
 			continue;
 		}
-
-		char buf[1024];
-		size_t dstlen = sizeof(buf);
-
-		Utf16_To_Utf8(buf, &dstlen, tempBuf, written);
 
 		FileData fd;
-		fd.origName = buf;
+		fd.origName = std::move(fileName.value());
 		fd.fp = i;
 		fd.size = SzArEx_GetFileSize(&db, i);
-		fd.crc = 0;  //; (f->Size > 0) ? f->Crc : 0;
 		if (SzBitWithVals_Check(&db.Attribs, i)) {
 			// LOG_DEBUG("%s %d", fd.origName.c_str(), db.Attribs.Vals[i])
 			if (db.Attribs.Vals[i] & 1 << 16)
@@ -167,21 +171,21 @@ CSevenZipArchive::CSevenZipArchive(const std::string& name)
 			else
 				fd.mode = 0644;
 		}
-		fileData.push_back(fd);
+
+		fileData.emplace_back(std::move(fd));
 	}
 }
 
 CSevenZipArchive::~CSevenZipArchive()
 {
-	if (outBuffer) {
+	if (outBuffer != nullptr) {
 		IAlloc_Free(&allocImp, outBuffer);
 	}
 	if (isOpen) {
 		File_Close(&archiveStream.file);
-		ISzAlloc_Free(&allocImp, lookStream.buf);
 	}
+	ISzAlloc_Free(&allocImp, lookStream.buf);
 	SzArEx_Free(&db, &allocImp);
-	SzFree(nullptr, tempBuf);
 }
 
 unsigned int CSevenZipArchive::NumFiles() const
@@ -214,9 +218,4 @@ void CSevenZipArchive::FileInfo(unsigned int fid, std::string& name, int& size, 
 	name = fileData[fid].origName;
 	size = fileData[fid].size;
 	mode = fileData[fid].mode;
-}
-
-unsigned int CSevenZipArchive::GetCrc32(unsigned int fid)
-{
-	return fileData[fid].crc;
 }
