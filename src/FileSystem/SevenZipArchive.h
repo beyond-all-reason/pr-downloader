30c4f2a0837bd7f0e0fa9cebefd4544b561b8680
Marek Rusinowski <marekrusinowski@gmail.com>
2022-12-29 20:03:20 +0100

    Update 7z lib to latest version 22.01 and improve usage
    - Makes sure that there isn't any buffer overflow possible in Utf16_To_Utf8
    - Align implementation more with 7zMain
    - Change 7z CMake target name to 7zip

--- src/FileSystem/SevenZipArchive.h
+++ src/FileSystem/SevenZipArchive.h
@@ -2,11 +2,8 @@
 
 #pragma once
 
-extern "C" {
-#include "lib/7z/7z.h"
-#include "lib/7z/7zFile.h"
-#include "lib/7z/7zTypes.h"
-}
+#include <7z.h>
+#include <7zFile.h>
 
 #include "IArchive.h"
 #include <string>
@@ -24,51 +21,27 @@ public:
 	virtual unsigned int NumFiles() const override;
 	virtual bool GetFile(unsigned int fid, std::vector<unsigned char>& buffer) override;
 	virtual void FileInfo(unsigned int fid, std::string& name, int& size, int& mode) const override;
-	virtual unsigned GetCrc32(unsigned int fid);
 
 private:
-	UInt32 blockIndex = 0xFFFFFFFF;
-	Byte* outBuffer = nullptr;
-	size_t outBufferSize = 0;
-
 	struct FileData {
 		int fp;
 		/**
 		 * Real/unpacked size of the file in bytes.
-		 * @see #unpackedSize
-		 * @see #packedSize
 		 */
 		int size;
 		std::string origName;
-		unsigned int crc;
-		/**
-		 * How many bytes of files have to be unpacked to get to this file.
-		 * This either equal to size, or is larger, if there are other files
-		 * in the same solid block.
-		 * @see #size
-		 * @see #packedSize
-		 */
-		int unpackedSize;
-		/**
-		 * How many bytes of the archive have to be read
-		 * from disc to get to this file.
-		 * This may be smaller or larger then size,
-		 * and is smaller then or equal to unpackedSize.
-		 * @see #size
-		 * @see #unpackedSize
-		 */
-		int packedSize;
+
 		/**
 		 * file mode
 		 */
 		int mode;
 	};
-	int GetFileName(const CSzArEx* db, int i);
-	const char* GetErrorStr(int res);
 
 	std::vector<FileData> fileData;
-	UInt16* tempBuf = nullptr;
-	size_t tempBufSize = 0;
+
+	UInt32 blockIndex = 0xFFFFFFFF;
+	Byte* outBuffer = nullptr;
+	size_t outBufferSize = 0;
 
 	CFileInStream archiveStream;
 	CSzArEx db;
@@ -76,5 +49,5 @@ private:
 	ISzAlloc allocImp;
 	ISzAlloc allocTempImp;
 
-	bool isOpen;
+	bool isOpen = false;
 };
